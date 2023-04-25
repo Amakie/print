e2100f4487634316d152bc6bbfce1ae11bc7250e
root <consamate57@gmail.com>
2023-04-25 13:55:28 -0700

    integr specifier

--- int_conversion.c
+++ int_conversion.c
@@ -1,46 +1,42 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * _printf - conversion specifier for integer
- * ...: variadic function
- * @format: string
+ * int_conversion - conversion specifier for integer
+ * @args: input string
+ * @str: string
+ * @n: integer
  * Return: number of characters printed
  */
-int _printf(const char *format, ...)
+int int_conversion(va_list args, char *str, unsigned int n)
 {
-	va_list mylist;
-	int num_chars = 0;
-	int value = 0;
+	int int_add;
+	unsigned int int_ad, a, int_tmp, neg, divide;
 
-	va_start(mylist, format);
-
-	for (; *format != '\0'; format++)
+	int_add = va_arg(args, int);
+	neg = 0;
+	if (int_add < 0)
 	{
-		if (*format == '%')
-		{	format++;
-			if (*format == 'd' || *format == 'i')
-			{
-
-				value += va_arg(mylist, int);
-				printf("%d", value);
-				num_chars++;
-				value = 0;
-			}
-
-			else
-			{
-				printf("unhandled specifier: %%%c", *format);
-			}
-		}
-		else
-		{
-			putchar(*format);
-			num_chars++;
-		}
+		int_ad = int_add * -1;
+		n = handle_buf(str, '-', n);
+		neg = 1;
+	}
+	else
+	{
+		int_ad = int_add;
 	}
 
-	va_end(mylist);
-	return (num_chars);
+	int_tmp = int_ad;
+	divide = 1;
+
+	while (int_tmp > 9)
+	{
+		divide *= 10;
+		int_tmp /= 10;
+	}
+
+	for (a = 0; divide > 0; divide /= 10, a++)
+	{
+		n = handle_buf(str, ((int_ad / divide) % 10) + '0', n);
+	}
+	return (a + neg);
 }
--- main.h
+++ main.h
@@ -3,7 +3,23 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct print_func - struct for print functions
+ * @struct_args: conversion specifier
+ * @ptr: pointer to the print functions
+ * Desc: this struct stores pointers to the print functions
+ */
+
+typedef struct print_func
+{
+	char *struct_args;
+	int (*ptr)(va_list, char *, unsigned int);
+} print_f;
 
 int _printf(const char *format, ...);
+int int_conversion((va_list args, char *str, unsigned int n);
 
 #endif
