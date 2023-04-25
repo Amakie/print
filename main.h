b421ae2c6698e27dad8cf513ce986f21589697c4
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-04-25 05:55:38 -0700

    integer specifiers

--- main.h
+++ main.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
-
+int int_spec(const char *str, int len, const char spec);
 #endif
